3ccc51612dcaa14f62383d228a5021bb180178d7
Diegodkid <diegodkid@gmail.com>
2021-12-22 10:57:25 -0400

    base64 key

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -2,11 +2,15 @@ import { GoogleSpreadsheet } from 'google-spreadsheet'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
+const fromBase64 = value => {
+   const buff = Buffer.from(value, 'base64');
+   return buff.toString('ascii');
+}
+
 export default async (req, res) => {
-   //await doc.useServiceAccountAuth(credentials)
    await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
-      private_key: process.env.SHEET_PRIVATE_KEY
+      private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
    })
    await doc.loadInfo()
 
