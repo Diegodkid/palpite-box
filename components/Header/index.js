0fe823c90258b605e962153aa532caba146e5a68
Diegodkid <diegodkid@gmail.com>
2021-12-23 16:02:37 -0400

    text

--- components/Header/index.js
+++ components/Header/index.js
@@ -16,13 +16,13 @@ const Header = () => {
       </div>
       <div className="bg-[#000409] p-4 text-blue-200 shadow-md font-bold text-center">
          <Link href='/sobre'>
-            <a className="px-5 md:px-10 hover:underline">Sobre</a>
+            <a className="px-5 text-lg md:px-10 hover:underline">Sobre</a>
          </Link>
          <Link href='/contato'>
-            <a className="px-5 md:px-10 hover:underline">Contato</a>
+            <a className="px-5 text-lg md:px-10 hover:underline">Contato</a>
          </Link>
          <Link href='/pesquisa'>
-            <a className="px-5 md:px-10 hover:underline">Pesquisa</a>
+            <a className="px-5 text-lg md:px-10 hover:underline">Pesquisa</a>
          </Link>
       </div>
    </React.Fragment>   
